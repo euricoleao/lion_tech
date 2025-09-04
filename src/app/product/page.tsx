541fde363ea1b08bbc5af2438c247605709293db
euricoLeao <euricobig@gmail.com>
2025-09-03 21:49:37 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ src/app/product/page.tsx
@@ -0,0 +1,9 @@
+const Product = () => {
+  return (
+    <div className="mt-2 flex items-center justify-center p-4 text-red-700">
+      <h1> Product Page</h1>
+    </div>
+  );
+};
+
+export default Product;
