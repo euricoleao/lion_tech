eba1690d1cd5ea9c0a0563705286aa6966272eee
euricoLeao <euricobig@gmail.com>
2025-09-05 19:22:45 -0300

    chore: add drizzle setup with initial schema:add seed script

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Product = () => {
   return (
-    <div className="mt-2 flex items-center justify-center p-4 text-red-700">
+    <div className="mt-2 flex flex-col items-center justify-center gap-4 p-4 text-red-700">
       <h1> Product Page</h1>
+      <Button className="">Cadastrar</Button>
     </div>
   );
 };
